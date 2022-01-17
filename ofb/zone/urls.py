1b5d5606aaee4aefa7616ebde0c239744ecbf97e
qazpllp <qazpllp4665@hotmail.com>
2022-01-17 17:33:13 +0000

    basic views

--- ofb/zone/urls.py
+++ ofb/zone/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('author/<int:pk>/', views.DetailAuthorView.as_view(), name='author'),
 ]
--- ofb/zone/views.py
+++ ofb/zone/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
+from .models import Story, Author
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    context_object_name = 'latest_story_list'
+
+    def get_queryset(self):
+        """
+        Returns the last 5 piblished stories
+        """
+        return Story.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Story
+
+class DetailAuthorView(generic.DetailView):
+    model = Author
