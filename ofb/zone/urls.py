5dfe1a3ef93de8d0613a20a5f0c30a401fc12fb3
qazpllp <qazpllp4665@hotmail.com>
2022-01-18 12:02:52 +0000

    Creating views

--- ofb/zone/urls.py
+++ ofb/zone/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('stories/', views.StoriesView.as_view(), name='stories'),
+    path('stories/<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('author/<int:pk>/', views.DetailAuthorView.as_view(), name='author'),
+    path('tags', views.TagListView.as_view(), name='tags'),
 ]
--- ofb/zone/views.py
+++ ofb/zone/views.py
@@ -6,6 +6,16 @@ from django.views import generic
 from .models import Story, Author
 
 class IndexView(generic.ListView):
+    template_name = 'zone/index.html'
+    context_object_name = 'latest_story_list'
+
+    def get_queryset(self):
+        """
+        Returns the last 5 piblished stories
+        """
+        return Story.objects.order_by('-pub_date')[:5]
+
+class StoriesView(generic.ListView):
     context_object_name = 'latest_story_list'
 
     def get_queryset(self):
@@ -27,3 +37,7 @@ class DetailAuthorView(generic.DetailView):
         context = super(DetailAuthorView, self).get_context_data(*args, **kwargs)
         context["story_list"] = Story.objects.filter(author__id = self.kwargs['pk'])
         return context
+
+class TagListView(generic.ListView):
+    template_name = 'zone/tags.html'
+    model = Story
