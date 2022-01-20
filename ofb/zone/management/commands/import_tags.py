24ae0de404addcf2d921d30c3cb56161b5ea3a6a
qazpllp <qazpllp4665@hotmail.com>
2022-01-20 07:26:28 +0000

    import tag descriptions

---
+++ ofb/zone/management/commands/import_tags.py
@@ -0,0 +1,20 @@
+import csv
+from django.core.management import BaseCommand
+from zone.models import Tag
+
+class Command(BaseCommand):
+    help = 'import a csv of the generated site tag information'
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file_path')
+    
+    def handle(self, *args, **options):
+        with open(options['csv_file_path']) as f:
+                reader = csv.reader(f)
+                next(reader, None) # skip header
+                for row in reader:
+                    if row[1]:
+                        tag, created_t = Tag.objects.update_or_create(
+                            slug = row[0],
+                            defaults = {'description': row[1]}
+                        )
