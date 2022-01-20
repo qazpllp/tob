f3f857f22c65ac2700c81d65d5f7fdc218746d93
qazpllp <qazpllp4665@hotmail.com>
2022-01-20 21:49:06 +0000

    filter minor improvements

--- ofb/zone/models.py
+++ ofb/zone/models.py
@@ -5,7 +5,7 @@ class Tag(models.Model):
     description = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.slug
+        return self.description
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
