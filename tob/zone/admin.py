cd6437c7a1f11af908a23bfc147bb0a5448b6b21
qazpllp <qazpllp4665@hotmail.com>
2022-01-21 15:17:27 +0000

    Missed a couple of files

--- tob/zone/admin.py
+++ tob/zone/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Story, Tag
+
+admin.site.register(Author)
+admin.site.register(Story)
+admin.site.register(Tag)
