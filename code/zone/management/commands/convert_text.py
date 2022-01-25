c449f8bb6e0b3040dba224e9c4df8d9aa39445ff
qazpllp <qazpllp4665@hotmail.com>
2022-01-25 10:17:51 +0000

    fixes for converting text

--- code/zone/management/commands/convert_text.py
+++ code/zone/management/commands/convert_text.py
@@ -10,43 +10,53 @@ import markdown
 from zone.models import Author, Story, Tag
 
 class Command(BaseCommand):
-    help = "Take text and convert it to various static files for better reading"
+	help = "Take text and convert it to various static files for better reading"
 
-    # html, pdf, epub
+	# html, pdf, epub
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--story_id', 
-            help='Convert only a single story',
-        )
+	def add_arguments(self, parser):
+		parser.add_argument(
+			'--story_id', 
+			help='Convert only a single story',
+		)
+		parser.add_argument(
+			'--forced', 
+			action="store_true", 
+			help="Overwrite already stored data")
 
-    def handle(self, *args, **options):
+	def handle(self, *args, **options):
 
-        for s in Story.objects.all():
-            if options['story_id'] and s.id != int(options['story_id']):
-                continue
+		for s in Story.objects.all():
+			if options['story_id'] and s.id != int(options['story_id']):
+				continue
 
-            # story_dir = os.path.join(static('zone/stories/'), str(s.id))
-            story_dir = os.path.join('zone/static/zone/stories/', str(s.id))
-            Path(story_dir).mkdir(parents=True, exist_ok=True)
+			# story_dir = os.path.join(static('zone/stories/'), str(s.id))
+			story_dir = os.path.join('zone/static/zone/stories/', str(s.id))
+			Path(story_dir).mkdir(parents=True, exist_ok=True)
 
-            # pdf 
-            options = {
-                'page-size': 'Letter',
-                'margin-top': '0.75in',
-                'margin-right': '0.75in',
-                'margin-bottom': '0.75in',
-                'margin-left': '0.75in',
-                'encoding': "UTF-8",
-                'no-outline': None
-            }
+			# pdf 
+			pdf_name = os.path.join(story_dir,str(s.id)+".pdf")
+			if not os.path.exists(pdf_name) or (os.path.exists(pdf_name) and options['forced']):
+				print(f"Converting {s} to pdf")
 
-            # format newlines for html
-            pdfkit.from_string(s.text.replace("\n", "<br>"), os.path.join(story_dir,str(s.id)+".pdf"), options=options)
-            
-            # html
-            html = markdown.markdown(s.text)
-            with open(os.path.join(story_dir,str(s.id)+".html"), 'w') as f:
-                f.write(html)
+				opts = {
+					'page-size': 'Letter',
+					'margin-top': '0.75in',
+					'margin-right': '0.75in',
+					'margin-bottom': '0.75in',
+					'margin-left': '0.75in',
+					'encoding': "UTF-8",
+					'no-outline': None
+				}
 
-            break
+				# format newlines for html
+				pdfkit.from_string(s.text.replace("\n", "<br>"), pdf_name, options=opts)
+			
+			# html
+			html_name = os.path.join(story_dir,str(s.id)+".html")
+			if not os.path.exists(html_name) or (os.path.exists(html_name) and options['forced']):
+				print(f"Converting {s} to html")
+
+				html = markdown.markdown(s.text)
+				with open(html_name, 'w') as f:
+					f.write(html)
