37f5ea3914f94fefe959a212e6f6bf5cb7df350c
qazpllp <qazpllp4665@hotmail.com>
2022-01-26 07:32:03 +0000

    heroku changes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 # syntax=docker/dockerfile:1
 FROM python:3
+
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
+ENV DEBUG=0
 
 # Install dependencies
 RUN apt-get update \
@@ -10,6 +12,18 @@ RUN apt-get update \
     && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /code
-COPY requirements.txt /code/
+COPY ./requirements.txt .
 RUN pip install -r requirements.txt
+
+# Copy project code
 COPY ./code /code/
+
+# collect static files
+RUN python manage.py collectstatic --noinput
+
+# add and run as non-root user
+# RUN adduser -D userfoo
+# USER userfoo
+
+# run gunicorn
+# CMD gunicorn tob.wsgi:application --bind 0.0.0.0:$PORT
--- code/tob/settings.py
+++ code/tob/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import sqlite3
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4k@_&u$x(%pddx(_v@iqik2^@9o%u$8mjg#bk87iy)xrci37h9'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-4k@_&u$x(%pddx(_v@iqik2^@9o%u$8mjg#bk87iy)xrci37h9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -47,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,7 +127,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Compression and caching support
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
